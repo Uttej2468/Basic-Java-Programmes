82bacc062bc75cb27d9cf449cf121f8bdd8a242b
Uttej2468 <54265691+Uttej2468@users.noreply.github.com>
2021-05-02 17:33:55 +0530

    Add files via upload

--- MethodDemo.java
+++ MethodDemo.java
@@ -1,29 +1,27 @@
-import java.util.*;
 class MethodDemo
 {
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the three numbers");
-		 int n1=sc.nextInt();
-		 int n2=sc.nextInt();
-		 int n3=sc.nextInt();
-		 reverse(n1);
-		 reverse(n2);
-		 reverse(n3);
-
-		 
-	}
-public static void reverse(int m){
-		int r,s=0;
-	while(m>0)
-		 {
-		 	r=m%10;
-		    s=s*10+r;
-		    m=m/10;
-		 }
-		 System.out.println(s);
-
+ 
+static void add() //method definition
+{
+	int a=5;
+	int b=7;
+	System.out.println(a+b);
+	
+}
+void sub() //method definition
+{
+	int a=5;
+	int b=7;
+	System.out.println(a-b);
+}
+public static void main(String args[])
+{
+	MethodDemo md=new MethodDemo();
+	add();
+	md.sub();
+	MethodDemo1 md1=new MethodDemo1();
+	md1.sumofN();
 }
 
 }
+
--- Pattern1.java
+++ Pattern1.java
@@ -1,23 +1,50 @@
 import java.util.*;
 class Pattern1
 {
-	 public static void main(String[] args)
+	public static void main(String args[])
 	{
-	int n,a=1;
-	Scanner sc=new Scanner(System.in);
-	n=sc.nextInt();
-	for(int i=0;i<=n;i++)
-
-
-	{
-
-	for(int j=1;j<=i;j++)
-
-	{
-		System.out.print(i+"");
-	}
-	System.out.println();
-
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		int x=1,y=n-1;
+		for(int i=1;i<=n;i++)
+		{
+			if(i%2==0)
+			{
+				for(int j=y;j>0;j--)
+				{
+					System.out.print("#");
+				}
+				for(int b=0;b<x;b++)
+					
+					{
+						System.out.print("*");
+					}
+				
+			}
+			else
+			{
+				for(int a=0;a<x;a++)
+				{
+				System.out.print("*");
+				}
+				for(int j=y;j>0;j--)
+				{
+				System.out.print("#");
+				}
+				
+				
+			}
+			if(i>=(n/2)+1)
+		{
+        	y++;
+			x--;
+		}
+			else 
+			{
+			   x++;
+			   y--;
+			}
+			System.out.println("");
 		}
 	}
 }
